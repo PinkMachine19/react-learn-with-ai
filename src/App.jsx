5df4a4bd761cfb39f308fcbabb34512824a839b2
Othniel <odarlingampofo@gmail.com>
2026-07-03 18:10:36 -0500

    session-10: first CountryCard component function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import CountryCard from "./component/CountryCard";
+
 function App() {
-  const countryName = 'canada';
-  const Mathresult = 2+2; 
   return (
     <div>
-      <h1>Hello world</h1>
-      <p>Country: {countryName}</p>
-      <p>2+2 : {Mathresult}</p>
+      <h1>Countries</h1>
+      <CountryCard/>
     </div>
   );
 }
---
+++ src/component/CountryCard.jsx
@@ -0,0 +1,8 @@
+function CountryCard() {
+  return (<div className="country-card">
+     <h2> Canada</h2>
+     <p>Region: Americas</p> 
+  </div>);
+}
+
+export default CountryCard
