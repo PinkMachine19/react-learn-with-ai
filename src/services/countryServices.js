24224e923c50d08ddda2f778708e649d0cc3e71a
Othniel <odarlingampofo@gmail.com>
2026-08-08 19:20:45 -0500

    session-25: simulate asynchronous country responses

--- src/services/countryServices.js
+++ src/services/countryServices.js
@@ -1,15 +1,26 @@
 /*Create src/services/countryService.js with synchronous functions that encapsulate access to country data.*/
 
+/*lab objective: Convert countryService.js to return delayed Promises and verify the functions with async/await and error handling. */
+
 import countries from '../data/countries'
 
-export function getCountries() {
+function delay(ms) {
+  return new Promise (resolve => setTimeout(resolve,ms))
+}
+
+export async function getCountries() {
+  await delay(300)
   return[...countries]
 }
-
-export function getCountryById(id) {
-  return countries.find(c=> c.id === id)
+export async function getCountryById(id) {
+  await delay(150)
+  const country = countries.find(c=> c.id ===id)
+  if(!country) throw Error (`Country ${country} Not Found`);
+  return country    
 }
 
-export function getCountriesByRegion(region) {
-  return countries.filter(c => c.region === region)
+export async function getCountriesByRegion(region){
+  await delay(200)
+  return countries.filter(c=> c.region === region)
 }
+
---
+++ src/services/test.js
@@ -0,0 +1,5 @@
+function print (){
+  console.log('HI')
+}
+
+setTimeout(print,10000)
