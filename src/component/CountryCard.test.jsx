cdbda89d784f93acf2e1d52b3c7754705d648017
Othniel <odarlingampofo@gmail.com>
2026-08-15 18:42:17 -0500

    session-29: test CountryCard props and output

--- src/component/CountryCard.test.jsx
+++ src/component/CountryCard.test.jsx
@@ -1,7 +1,27 @@
-import {render} from "@testing-library/react"
-import {test} from "vitest"
+import {render,screen} from "@testing-library/react"
+import {test,expect} from "vitest"
 import CountryCard from "./CountryCard.jsx"
 
 test('renders Country card with valid props', () => {
   render(<CountryCard name="Japan" region="Asia"/>)
+
+  expect(
+    screen.getByRole('heading', {name: "Japan"})
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByText("Region:Asia", {exact:true})
+  ).toBeInTheDocument()
 })
+
+test('does not render unrelated country details', () => {
+  render(<CountryCard name="Japan" region="Asia" />);
+
+expect(
+  screen.queryByText('Ghana', { exact: true })
+).not.toBeInTheDocument();
+
+  expect(
+    screen.queryByText('Eurasia', { exact: true })
+  ).not.toBeInTheDocument();
+});
