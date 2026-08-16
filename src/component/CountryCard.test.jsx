d2607e3364d401ef2106db20617d58641ca95769
Othniel <odarlingampofo@gmail.com>
2026-08-16 18:50:29 -0500

    session-30: test CountryCard events and state output

--- src/component/CountryCard.test.jsx
+++ src/component/CountryCard.test.jsx
@@ -1,27 +1,42 @@
 import {render,screen} from "@testing-library/react"
 import {test,expect} from "vitest"
+import userEvent from "@testing-library/user-event"
 import CountryCard from "./CountryCard.jsx"
 
-test('renders Country card with valid props', () => {
+
+test('increment ', async () => {
+  const user = userEvent.setup();
   render(<CountryCard name="Japan" region="Asia"/>)
 
   expect(
-    screen.getByRole('heading', {name: "Japan"})
+    screen.getByText('Clicked 0 times', {exact:true})
   ).toBeInTheDocument();
 
-  expect(
-    screen.getByText("Region:Asia", {exact:true})
-  ).toBeInTheDocument()
-})
+  const increment = screen.getByRole('button', {name: /click me/i})
 
-test('does not render unrelated country details', () => {
-  render(<CountryCard name="Japan" region="Asia" />);
-
-expect(
-  screen.queryByText('Ghana', { exact: true })
-).not.toBeInTheDocument();
+  await user.click(increment);
+  await user.click(increment);
+  await user.click(increment);
 
   expect(
-    screen.queryByText('Eurasia', { exact: true })
-  ).not.toBeInTheDocument();
+    screen.getByText('Clicked 3 times', {exact:true})
+  ).toBeInTheDocument
+
+
+ 
 });
+
+test("rest only" , async () => {
+  const user = userEvent.setup();
+  render(<CountryCard name="Japan" region="Asia"/>)
+  const reset = screen.getByRole('button', {name:/reset/i})
+
+  await user.click(reset)
+
+  expect(
+    screen.getByText('Clicked 0 times', {exact:true})
+  ).toBeInTheDocument
+}
+  
+)
+
