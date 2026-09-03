b92084e25303838330da8b919fa7679a2a947448
Othniel <odarlingampofo@gmail.com>
2026-09-03 18:30:37 -0500

    session-37: useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,31 @@ import CountryList from "./component/CountryList.jsx"
 import countries from './features/countries/countries';
 import useCountrySearch from './features/countries/useCountrySearch.js';
 import Button from './component/ui/button.jsx';
-
+import {useEffect} from "react"
 
 /* Lab objective: Move searchTerm, its handler, and the <input> from CountryList to App. Filter countries in App and pass the result down as a countries prop. */
 
 function App() {
  
 
-const {
-  searchTerm,
-  handleSearchTerm,
-  filteredCountries,
-  resultCount,
-  clearSearch
-} = useCountrySearch(countries);
+  const {
+    searchTerm,
+    handleSearchTerm,
+    filteredCountries,
+    resultCount,
+    clearSearch
+ } = useCountrySearch(countries);
+
+  useEffect(()=> {
+    document.title = `${resultCount} countries`;
+  }), ([resultCount])
+
+  useEffect(() => {
+  const onKey = e => e.key === 'Escape' && clearSearch();
+  window.addEventListener('keydown', onKey);
+  return () => window.removeEventListener('keydown', onKey);
+  }, [clearSearch]);
+
   
   return (
     
